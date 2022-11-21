0c6c044beaae81128829f712e52d36e4844cde7e
alybally04 <43581966+alybally04@users.noreply.github.com>
2022-11-21 11:28:13 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,30 +5,37 @@ dpg.create_context()
 
 
 def convert_video():
+    if dpg.does_alias_exist('formats_table'):
+        dpg.delete_item('formats_table')
+
+    dpg.add_loading_indicator(tag='loading_indicator', parent='main')
     url_input = dpg.get_value('url_input')
 
-    with YoutubeDL({'listformats': True}) as ydl:
-        info = ydl.extract_info(url_input, download=False)
+    '''with YoutubeDL({'listformats': True}) as ydl:
+        formats = ydl.extract_info(url_input, download=False)'''
 
-    dpg.add_text('nigga', parent='main')
+    with YoutubeDL({}) as ydl:
+        video_data = ydl.extract_info(url_input, download=False)
+        print(video_data)
 
-    with dpg.table(header_row=False, parent='main'):
+    dpg.delete_item('loading_indicator')
+    with dpg.table(tag='formats_table', parent='main', row_background=True):
 
         # use add_table_column to add columns to the table,
         # table columns use child slot 0
-        dpg.add_table_column()
-        dpg.add_table_column()
-        dpg.add_table_column()
+        # by default the tag is used as label unless specified otherwise
+        dpg.add_table_column(label='Format')
+        dpg.add_table_column(label='Quality')
+        dpg.add_table_column(label='Size')
+        dpg.add_table_column(enabled=False)
 
-        # add_table_next_column will jump to the next row
-        # once it reaches the end of the columns
-        # table next column use slot 1
-        for i in range(0, 4):
-            with dpg.table_row():
-                for j in range(0, 3):
-                    dpg.add_text(f"Row{i} Column{j}")
-
-    print(info)
+        # Parse available formats
+        for video_format in video_data['formats']:
+            if video_format['ext'] != 'webm':
+                with dpg.table_row():
+                    # Do not list webm format type
+                    dpg.add_text(video_format['ext'])
+                    dpg.add_text(video_format['format_note'])
 
 
 with dpg.window(tag='main'):
@@ -36,7 +43,7 @@ with dpg.window(tag='main'):
     dpg.add_input_text(label="URL", tag='url_input', no_spaces=True)
     dpg.add_button(label="Convert Video", callback=convert_video)
 
-dpg.create_viewport(title='NoHotMILFs', width=400, height=600)
+dpg.create_viewport(title='NoHotMILFs', width=400, height=500)
 dpg.setup_dearpygui()
 
 dpg.show_viewport()
