6d7502153400ebcfcad1dce7ade714833d52f20d
alybally04 <43581966+alybally04@users.noreply.github.com>
2022-11-21 13:31:14 +0000

    Update app.py and .gitignore

--- app.py
+++ app.py
@@ -4,15 +4,35 @@ from youtube_dl import YoutubeDL
 dpg.create_context()
 
 
+# Convert bits into Human-readable format
+def filesize_readable(bits):
+    if bits < 1000:  # bits
+        units = 'bytes'
+    elif bits < 1000000:  # kilobits
+        bits /= 1000
+        units = 'KB'
+    elif bits < 1000000000:  # megabits
+        bits /= 1000000
+        units = 'MB'
+    else:  # gigabits
+        bits /= 1000000000
+        units = 'GB'
+
+    # Convert from bits to bytes
+    bits /= 8
+
+    # Format the output
+    formatted_string = f'{round(bits, 1)} {units}'
+
+    return formatted_string
+
+
 def convert_video():
     if dpg.does_alias_exist('formats_table'):
         dpg.delete_item('formats_table')
 
     dpg.add_loading_indicator(tag='loading_indicator', parent='main')
     url_input = dpg.get_value('url_input')
-
-    '''with YoutubeDL({'listformats': True}) as ydl:
-        formats = ydl.extract_info(url_input, download=False)'''
 
     with YoutubeDL({}) as ydl:
         video_data = ydl.extract_info(url_input, download=False)
@@ -31,11 +51,18 @@ def convert_video():
 
         # Parse available formats
         for video_format in video_data['formats']:
-            if video_format['ext'] != 'webm':
+            if video_format['ext'] != 'webm' and video_format['filesize'] is not None:
                 with dpg.table_row():
                     # Do not list webm format type
                     dpg.add_text(video_format['ext'])
-                    dpg.add_text(video_format['format_note'])
+
+                    if video_format['format_note'] == 'tiny':
+                        # Audio files are referred to as tiny for some reason
+                        dpg.add_text('Audio only')
+                    else:
+                        dpg.add_text(video_format['format_note'])
+
+                    dpg.add_text(filesize_readable(video_format['filesize']))
 
 
 with dpg.window(tag='main'):
