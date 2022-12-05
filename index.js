8fe06ec57c148d6c8a32b7e1a062b8684c870c5a
alybally04 <43581966+alybally04@users.noreply.github.com>
2022-12-05 20:47:34 +0000

    Further work on UI

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const { app, BrowserWindow } = require('electron');
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1000,
+    height: 800,
+    minWidth: 800,
+    minHeight: 600,
   });
 
   win.loadFile('index.html');
