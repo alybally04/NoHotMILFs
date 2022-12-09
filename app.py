811f95f6fa1763e28c993f63c72f372b6df8567d
alybally04 <43581966+alybally04@users.noreply.github.com>
2022-12-09 18:58:05 +0000

    Begun work on appinterface.js and app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-import platform
+print('bussy')
 
+'''import platform
 import yt_dlp.utils
 from yt_dlp import YoutubeDL
 
@@ -62,3 +63,60 @@ def time_readable(num1):
             duration = f'{hours}:0{minutes}:0{seconds}'
 
     return duration
+
+
+def lookup_video(url_input):
+    try:
+        with YoutubeDL({'quiet': True, 'noplaylist': True, 'cachedir': False}) as ydl:
+            video_data = ydl.extract_info(url_input, download=False)
+
+    except yt_dlp.utils.DownloadError:
+        pass
+        # dpg.add_text('ERROR CONVERTING VIDEO - Check the URL is correct and try again', tag='conversion_error',
+                    # parent='main', color=[255, 0, 0])
+
+
+def download_video(url, title, format_id, file_type):
+    if file_type == 'mp4':
+        pass
+        # dpg.add_text('Downloading video file...', tag='current_download', parent='main')
+    else:
+        pass
+        # dpg.add_text('Downloading audio file...', tag='current_download', parent='main')
+
+    def progress_hook(response):
+        if response['status'] == 'downloading':
+            downloaded_percent = (response["downloaded_bytes"] * 100) // response["total_bytes"]
+            downloaded_percent /= 100
+
+            # dpg.set_value("eta", f"ETA {time_readable(response['eta'])}")
+            # dpg.set_value("progress_bar", downloaded_percent)
+
+        elif response['status'] == 'finished':
+            pass
+            # dpg.set_value('current_download', 'Downloading audio file...')
+
+    if dpg.get_value('include_audio_best') is True:
+        # format_string = f'{format_id}+bestaudio[ext=m4a]'
+        format_string = f'{format_id}+bestaudio'
+
+    elif dpg.get_value('include_audio_worst') is True:
+        # format_string = f'{format_id}+worstaudio[ext=m4a]'
+        format_string = f'{format_id}+worstaudio'
+
+    else:
+        format_string = format_id
+
+    with YoutubeDL({
+        'quiet': True,
+        'noplaylist': True,
+        'cachedir': False,
+        'progress_hooks': [progress_hook],
+        'format': format_string,
+        'merge_output_format': file_type,
+        # Begin string with r for raw string and f for f-string
+        'outtmpl': f"{downloads_path}NoHotMILFs - '{title}'.{file_type}",
+        'updatetime': False
+    }) as ydl:
+
+        ydl.download(url)'''
--- appinterface.js
+++ appinterface.js
@@ -1,6 +1,18 @@
 const {PythonShell} = require('python-shell')
+const { app } = require('electron');
 
-PythonShell.runString('x=1+1;print(x)', null, function (err) {
+let options = {
+    mode: 'text',
+    pythonPath: ((process.platform === 'win32') ? 'venv\\Scripts\\python.exe' : 'venv/bin/python'),
+    pythonOptions: ['-u'], // get print results in real-time
+    scriptPath: '', //Path to directory of script
+    args: []
+};
+
+const pyshell = PythonShell.run('app.py', options, function (err, results) {
     if (err) throw err;
-    console.log('finished');
+    // results is an array consisting of messages collected during execution
+    console.log('results: %j', results);
 });
+
+
