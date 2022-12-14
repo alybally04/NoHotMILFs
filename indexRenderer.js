b15b3fb061fa8c7f80d960cb84c43539aca3288c
alybally04 <43581966+alybally04@users.noreply.github.com>
2022-12-14 22:03:12 +0000

    Began cleaning up and optimising page element generation

--- indexRenderer.js
+++ indexRenderer.js
@@ -1,28 +1,38 @@
 const {PythonShell} = require('python-shell')
+let pythonPath;
+
 
 // Setting containers to be invisible at launch
 window.onload = function() {
     document.querySelector('#loading-icon').style.display = 'none';
-    document.querySelector('#video-details').style.display = 'none';
-    document.querySelector('#formats-table').style.display = 'none';
+
+    // TODO: Change this before building!
+
+    // For when running in dev environment
+    pythonPath = ((process.platform === 'win32') ? 'venv\\Scripts\\python.exe' : 'venv/bin/python')
+
+    // For when building distributable
+    // pythonPath = ((process.platform === 'win32') ?  process.resourcesPath + '\\venv\\Scripts\\python.exe' : process.resourcesPath + '/venv/bin/python')
 };
 
 
 function lookupVideo() {
-    document.querySelector('#loading-icon').style.display = null;
-    document.querySelector('#video-details').style.display = 'none';
-    document.querySelector('#formats-table').style.display = 'none';
+    // Removing previous video info and formats table sections
+    const videoInfoSection = document.querySelector('#video-details')
+    videoInfoSection.innerHTML = '';
 
     const formatsTableSection = document.querySelector('#formats-table')
     formatsTableSection.innerHTML = '';
 
+    // Making the loading symbol visible
+    document.querySelector('#loading-icon').style.display = null;
+
+    // Getting the user input
     const url_input = document.querySelector('#input-field').value
 
     let options = {
     mode: 'text',
-    // TODO: Change this before building
-    // pythonPath: ((process.platform === 'win32') ?  process.resourcesPath + '\\venv\\Scripts\\python.exe' : process.resourcesPath + '/venv/bin/python'),
-    pythonPath: ((process.platform === 'win32') ? 'venv\\Scripts\\python.exe' : 'venv/bin/python'),
+    pythonPath: pythonPath,
     // Get print results in real-time
     pythonOptions: ['-u'],
     // Path to directory of script
@@ -30,15 +40,12 @@ function lookupVideo() {
     args: ['lookup_video', url_input]
     };
 
-    // let pyshell = PythonShell.run(__dirname + '/core.py', options, function (err, results) {
     let pyshell = PythonShell.run('core.py', options, function (err, results) {
         if (err) throw err;
-        // results is an array consisting of messages collected during execution
-        // console.log('results: %j', results);
     });
 
+    // Receives output from python script via print statements
     pyshell.on('message', function (message) {
-        // received a message sent from the Python script (a simple "print" statement)
         let parsedMessage = JSON.parse(message);
         if (parsedMessage.hasOwnProperty('error')) {
 
@@ -52,10 +59,39 @@ function lookupVideo() {
             videoDetails[2].innerHTML = '';
 
         } else {
+            const videoInfo = parsedMessage.videoInfo;
+            const formats = parsedMessage.formats;
 
-            let videoInfo = parsedMessage.videoInfo;
-            let formats = parsedMessage.formats;
+            // Generating video information section
+            const videoTitle = document.createElement('h3');
+            videoTitle.appendChild(document.createTextNode(videoInfo.title));
 
+            const duration = document.createElement('li')
+            duration.appendChild(document.createTextNode(`Length: ${videoInfo.duration}`))
+
+            const channel = document.createElement('li')
+            channel.appendChild(document.createTextNode(`Length: ${videoInfo.channel}`))
+
+            const uploadDate = document.createElement('li')
+            uploadDate.appendChild(document.createTextNode(`Length: ${videoInfo.uploadDate}`))
+
+            const infoList = document.createElement('ul');
+            infoList.appendChild(duration)
+            infoList.appendChild(channel)
+            infoList.appendChild(uploadDate)
+
+            const videoThumbnail = document.createElement('img');
+            videoThumbnail.src = videoInfo.thumbnail;
+            videoThumbnail.alt = "Youtube video thumbnail"
+
+            const infoDiv = document.createElement('div')
+            infoDiv.appendChild(videoTitle)
+            infoDiv.appendChild(infoList)
+
+            videoInfoSection.appendChild(videoThumbnail)
+            videoInfoSection.appendChild(infoDiv)
+
+            /*
             document.querySelector('#video-details h3').innerHTML = videoInfo.title;
             document.querySelector('#video-details img').src = videoInfo.thumbnail;
 
@@ -64,38 +100,33 @@ function lookupVideo() {
             videoDetails[0].innerHTML = `Length: ${videoInfo.duration}`;
             videoDetails[1].innerHTML = `Channel: ${videoInfo.channel}`;
             videoDetails[2].innerHTML = `Upload Date: ${videoInfo.uploadDate}`;
+             */
 
             // Generating formats table
-            // const table = document.querySelector('#formats-table table')
-            // const tableBody = document.querySelector('#formats-table tbody')
-
             const table = document.createElement("table");
             const tableHead = document.createElement('thead');
             const tableBody = document.createElement("tbody");
 
             // Generating tableHead
-            if (true) {
-                const row = document.createElement('tr');
+            const row = document.createElement('tr');
 
-                const cell1 = document.createElement('th');
-                cell1.appendChild(document.createTextNode('Format'));
-                row.appendChild(cell1);
+            const cell1 = document.createElement('th');
+            cell1.appendChild(document.createTextNode('Format'));
+            row.appendChild(cell1);
 
-                const cell2 = document.createElement('th');
-                cell2.appendChild(document.createTextNode('Quality'));
-                row.appendChild(cell2);
+            const cell2 = document.createElement('th');
+            cell2.appendChild(document.createTextNode('Quality'));
+            row.appendChild(cell2);
 
-                const cell3 = document.createElement('th');
-                cell3.appendChild(document.createTextNode('Size'));
-                row.appendChild(cell3);
+            const cell3 = document.createElement('th');
+            cell3.appendChild(document.createTextNode('Size'));
+            row.appendChild(cell3);
 
-                // No text as button column
-                const cell4 = document.createElement('th');
-                row.appendChild(cell4);
+            // No text as button column
+            const cell4 = document.createElement('th');
+            row.appendChild(cell4);
 
-                tableBody.appendChild(row)
-            }
-
+            tableHead.appendChild(row)
 
             // Generating tableBody
             for (let count = 0; count < formats.length; count++) {
@@ -128,6 +159,7 @@ function lookupVideo() {
                 }
                 tableBody.appendChild(row)
             }
+            table.appendChild(tableHead)
             table.appendChild(tableBody)
             formatsTableSection.appendChild(table);
         }
@@ -140,9 +172,8 @@ function lookupVideo() {
         console.log('The exit signal was: ' + signal);
         console.log('finished');
 
+        // Removing loading icon once loading is finished
         document.querySelector('#loading-icon').style.display = 'none';
-        document.querySelector('#video-details').style.display = null;
-        document.querySelector('#formats-table').style.display = null;
     });
 }
 
@@ -150,9 +181,7 @@ function lookupVideo() {
 function downloadVideo(url, title, format_id, file_type) {
     let options = {
     mode: 'text',
-    // TODO: Change this before building
-    // pythonPath: ((process.platform === 'win32') ?  process.resourcesPath + '\\venv\\Scripts\\python.exe' : process.resourcesPath + '/venv/bin/python'),
-    pythonPath: ((process.platform === 'win32') ? 'venv\\Scripts\\python.exe' : 'venv/bin/python'),
+    pythonPath: pythonPath,
     // Get print results in real-time
     pythonOptions: ['-u'],
     // Path to directory of script
